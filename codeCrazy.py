fda3d150964f947814175aef03fcdc8ed77708fc
xXPetSecXx <201691633@cegepsherbrooke.qc.ca>
2025-09-08 09:26:14 -0400

    Correction
    J'ai changé un peu le texte pour qu'il soit plus poli envers l'utilisateur

--- codeCrazy.py
+++ codeCrazy.py
@@ -2,10 +2,12 @@ variable1 = ""
 variable2 = ""
 
 
-variable1 = input("T'es qui toé? Donne moi ton prénom : ")
+variable1 = input("T'es qui toé? Tu vas me donner ton prénom : ")
 
 print (f"Votre prénom est : {variable1}")
 
-variable2 = input("T'es qui toé? Donne moi ton nom : ")
+variable2 = input("T'es qui toé? Tu vas me donner ton nom : ")
 
 print (f"Votre prénom est : {variable2}")
+
+
