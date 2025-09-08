64b0a34e28d31a1f5ec975d6d58117f3921e065b
xXPetSecXx <201691633@cegepsherbrooke.qc.ca>
2025-09-08 09:24:05 -0400

    Nom
    Le code demande maintenant le nom de l'utilisateur de la même façon que le prénom

--- codeCrazy.py
+++ codeCrazy.py
@@ -5,3 +5,7 @@ variable2 = ""
 variable1 = input("T'es qui toé? Donne moi ton prénom : ")
 
 print (f"Votre prénom est : {variable1}")
+
+variable2 = input("T'es qui toé? Donne moi ton nom : ")
+
+print (f"Votre prénom est : {variable2}")
