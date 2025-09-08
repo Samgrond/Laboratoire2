f623a61e449d217310d24fb55cfa4bd761014b76
xXPetSecXx <201691633@cegepsherbrooke.qc.ca>
2025-09-08 09:31:24 -0400

    1.0
    Première version du programme complet

--- codeCrazy.py
+++ codeCrazy.py
@@ -1,3 +1,5 @@
+import random
+
 variable1 = ""
 variable2 = ""
 
@@ -10,4 +12,6 @@ variable2 = input("T'es qui toé? Tu vas me donner ton nom : ")
 
 print (f"Votre prénom est : {variable2}")
 
+variable3 = random.randint(1, 110)
 
+print(f"Votre nom complet est {variable1} {variable2} et j'assume que tu as {variable3} ans")
